2ccc6a45df1fd56828e61f166ebae95b57dabaa9
revspace7777 <revspace7777@example.com>
2025-08-24 22:26:55 -0400

    fucking forms part 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,56 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Hidden form for Netlify build detection - applies to ALL pages */}
-        <form
-          name="quote-request"
-          data-netlify="true"
-          netlify
-          netlify-honeypot="bot-field"
-          hidden
-        >
-          {/* All your form fields */}
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-          <input type="tel" name="phone" />
-          <input type="text" name="typeOfMove" />
-          <input type="text" name="movingFromStreet" />
-          <input type="text" name="movingFromCity" />
-          <input type="text" name="movingFromState" />
-          <input type="text" name="movingFromZip" />
-          <input type="text" name="movingFromBedrooms" />
-          <input type="text" name="movingFromFloor" />
-          <input type="text" name="movingToStreet" />
-          <input type="text" name="movingToCity" />
-          <input type="text" name="movingToState" />
-          <input type="text" name="movingToZip" />
-          <input type="text" name="movingToBedrooms" />
-          <input type="text" name="movingToFloor" />
-          <textarea name="description"></textarea>
-          <input type="text" name="subject" />
-          
-          {/* Tracking fields */}
-          <input type="text" name="campaignid" />
-          <input type="text" name="adgroupid" />
-          <input type="text" name="keyword" />
-          <input type="text" name="matchtype" />
-          <input type="text" name="gclid" />
-          <input type="text" name="utm_source" />
-          <input type="text" name="utm_medium" />
-          <input type="text" name="utm_campaign" />
-          <input type="text" name="utm_adgroup" />
-          <input type="text" name="utm_term" />
-          <input type="text" name="utm_content" />
-          <input type="text" name="page_url" />
-          
-          {/* Honeypot */}
-          <div style={{ display: 'none' }}>
-            <label>
-              Don't fill this out if you're human: <input name="bot-field" />
-            </label>
-          </div>
-        </form>
-
         {children}
       </body>
     </html>
