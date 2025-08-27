2d0613368aecb89faf12dcb55309eb6e333a2998
revspace7777 <revspace7777@example.com>
2025-08-27 18:52:20 -0400

    syntax errors in some jsons

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // The webpack function is the key addition here
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      loader: 'json-loader',
+      type: 'javascript/auto'
+    });
+
+    return config;
+  },
 }
 
 export default nextConfig
